f7a53361f884452fe7887c992ad5e7c27f2343ac
Mark Anthony Aguilar <markanthonyaguilar@digits.ph>
2024-07-26 15:39:30 +0800

    add migrations

---
+++ database/migrations/2024_07_26_071945_adjust_orderlist_fields.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_lists', function (Blueprint $table) {
+            $table->dropColumn('digits_code');
+            $table->dropColumn('store_cost');
+            $table->dropColumn('srp');
+            $table->dropColumn('uom');
+            $table->dropColumn('brand');
+            $table->dropColumn('item_description');
+            $table->integer('item_master_id')->nullable()->after('part_number');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_lists', function (Blueprint $table) {
+            $table->string('item_description')->after('phone_number')->nullable();
+            $table->string('uom')->after('item_description')->default('PCS');
+            $table->string('brand')->after('uom')->default('APPLE');
+            $table->string('digits_code', 10)->after('uploaded_file')->nullable();
+            $table->decimal('store_cost')->after('phone_number')->nullable();
+            $table->decimal('srp')->after('store_cost')->nullable();
+            $table->dropColumn('item_master_id');
+        });
+    }
+};
